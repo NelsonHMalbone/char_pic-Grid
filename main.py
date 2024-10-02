50d2ba7d4726c273b4bc8d9f0858c270c7d513ed
Nelson Malbone <nhmalbone@icloud.com>
2024-10-02 17:28:47 -0400

    10-2 i still feel like im missing something here.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Character Picture Grid
+# automate the boring stuff chap 4 pg 108
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        print(grid[i][j], end='')
+print()
